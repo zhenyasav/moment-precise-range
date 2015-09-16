197c70706117b0b2dea826f1edcd0302d0186c00
Zhenya <zhenya@meteor.com>
2015-09-16 11:38:52 -0700

    added moment version constraint

--- package.js
+++ package.js
@@ -12,6 +12,6 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.use('momentjs:moment');
+  api.use('momentjs:moment@2.10.6');
   api.addFiles('moment-precise-range/readable-range.js');
 });
